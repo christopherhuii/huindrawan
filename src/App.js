ae9b93c8176abc1b9c49cbabf34dc5209ae96dea
Christopher Hui <christopher.kam.hui@gmail.com>
2018-05-22 23:09:17 -0700

    more styling stuff

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Hero from './Hero';
+import Countdown from './Countdown';
 import SectionTitleAndDescription from './SectionTitleAndDescription';
 import WeddingDetails from './WeddingDetails';
 import WeddingParty from './WeddingParty';
@@ -10,6 +11,7 @@ class App extends Component {
     return (
       <div className="app">
         <Hero />
+        <Countdown />
         <SectionTitleAndDescription
           title="the couple"
           description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
